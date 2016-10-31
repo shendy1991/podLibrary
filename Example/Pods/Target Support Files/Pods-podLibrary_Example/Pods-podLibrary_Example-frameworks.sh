47c0aa65aaabf9c47fd4c1352795febed4f2962c
shendeyuan <shendeyuan@souche.com>
2016-10-31 19:12:11 +0800

    init configs

--- Example/Pods/Target Support Files/Pods-podLibrary_Example/Pods-podLibrary_Example-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-podLibrary_Example/Pods-podLibrary_Example-frameworks.sh
@@ -84,8 +84,12 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "$BUILT_PRODUCTS_DIR/MJRefresh/MJRefresh.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/Masonry/Masonry.framework"
   install_framework "$BUILT_PRODUCTS_DIR/podLibrary/podLibrary.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "$BUILT_PRODUCTS_DIR/MJRefresh/MJRefresh.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/Masonry/Masonry.framework"
   install_framework "$BUILT_PRODUCTS_DIR/podLibrary/podLibrary.framework"
 fi
